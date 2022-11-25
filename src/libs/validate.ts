1fc12f89205f522db8c0bd2e3714f44287e89c4b
svonme <svon.me@gmail.com>
2022-11-25 19:56:04 +0800

    fix:修复 validate 中空数据的判断

--- src/libs/validate.ts
+++ src/libs/validate.ts
@@ -8,6 +8,21 @@ import "reflect-metadata";
 
 const requiredMetadataKey = Symbol("required");
 
+const isEmpty = function(value: any): boolean {
+  if (value) {
+    return false;
+  }
+  // 空字符串与0在实际业务中属于有意义的数据
+  if (_.isString(value) || value === 0) {
+    return false;
+  }
+  // 判断 undefined, null, NaN
+  if (_.isNil(value) || _.isNaN(value)) {
+    return true;
+  }
+  return false;
+}
+
 /** 参数装饰器，必填参数 */
 export const required = function (target: object, propertyKey: string | symbol, parameterIndex: number) {
   // @ts-ignore
@@ -25,7 +40,7 @@ export const validate = function (target: any, propertyName: string, descriptor:
     const requiredParameters: number[] = Reflect.getOwnMetadata(requiredMetadataKey, target, propertyName);
     if (requiredParameters) {
       for (const parameterIndex of requiredParameters) {
-        if (parameterIndex >= args.length || _.isEmpty(args[parameterIndex])) {
+        if (parameterIndex >= args.length || isEmpty(args[parameterIndex])) {
           const message = new Error("Missing required argument.");
           console.log(message);
           throw message;
